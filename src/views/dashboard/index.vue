574814e7605a6780fb69f6cd2932dcfb71b01db5
George <xz58@uw.edu>
2019-01-22 01:48:25 -0800

    1. Updated Dashboard 2. Updated Student Info Table 3. Updated Report Generator 4. Added Website Link

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,8 +1,44 @@
 <template>
-  <div class="dashboard-container">
-    <div class="dashboard-text">name:{{ name }}</div>
-    <div class="dashboard-text">roles:<span v-for="role in roles" :key="role">{{ role }}</span></div>
-  </div>
+<el-row :gutter="40" class="panel-group">
+  <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+    <div class="card-panel" to="/example/table">
+      <div class="card-panel-icon-wrapper icon-table">
+        <svg-icon icon-class="table" class-name="card-panel-icon" />
+      </div>
+      <div class="card-panel-description">
+        <div class="card-panel-text">
+          <router-link to="/table/index" tag="div">Student Info</router-link>
+        </div>
+      </div>
+    </div>
+  </el-col>
+  <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+    <div class="card-panel" to="/example/table">
+      <div class="card-panel-icon-wrapper icon-form">
+        <svg-icon icon-class="form" class-name="card-panel-icon" />
+      </div>
+      <div class="card-panel-description">
+        <div class="card-panel-text">
+          <router-link to="/form/index" tag="div">Report</router-link>
+        </div>
+      </div>
+    </div>
+  </el-col>
+  <el-col :xs="12" :sm="12" :lg="6" class="card-panel-col">
+    <div class="card-panel" to="/example/table">
+      <div class="card-panel-icon-wrapper icon-website">
+        <svg-icon icon-class="eye" class-name="card-panel-icon" />
+      </div>
+      <div class="card-panel-description">
+        <div class="card-panel-text">
+          <a href="http://educatingyoungeyes.org/" target="_blank">
+            EYE
+          </a>
+        </div>
+      </div>
+    </div>
+  </el-col>
+</el-row>
 </template>
 
 <script>
@@ -15,18 +51,86 @@ export default {
       'name',
       'roles'
     ])
+  },
+  methods: {
+    navigate() {
+      this.router.push('/table')
+    }
   }
 }
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-.dashboard {
-  &-container {
-    margin: 30px;
+  .panel-group {
+    margin-top: 18px;
+    .card-panel-col{
+      margin-bottom: 32px;
+    }
+    .card-panel {
+      height: 108px;
+      cursor: pointer;
+      font-size: 12px;
+      position: relative;
+      overflow: hidden;
+      color: #666;
+      background: #fff;
+      box-shadow: 4px 4px 40px rgba(0, 0, 0, .05);
+      border-color: rgba(0, 0, 0, .05);
+      &:hover {
+        .card-panel-icon-wrapper {
+          color: #fff;
+        }
+        .icon-table {
+          background: #40c9c6;
+        }
+        .icon-form {
+          background: #36a3f7;
+        }
+        .icon-website {
+          background: #f4516c;
+        }
+        .icon-shopping {
+          background: #34bfa3
+        }
+      }
+      .icon-people {
+        color: #40c9c6;
+      }
+      .icon-message {
+        color: #36a3f7;
+      }
+      .icon-money {
+        color: #f4516c;
+      }
+      .icon-shopping {
+        color: #34bfa3
+      }
+      .card-panel-icon-wrapper {
+        float: left;
+        margin: 14px 0 0 14px;
+        padding: 16px;
+        transition: all 0.38s ease-out;
+        border-radius: 6px;
+      }
+      .card-panel-icon {
+        float: left;
+        font-size: 32px;
+      }
+      .card-panel-description {
+        float: bottom;
+        font-weight: bold;
+        margin: 26px;
+        margin-left: 0px;
+        .card-panel-text {
+          line-height: 54px;
+          color: rgba(0, 0, 0, 0.45);
+          font-size: 24px;
+          margin-bottom: 6px;
+        }
+        .card-panel-num {
+          font-size: 20px;
+        }
+      }
+    }
   }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
-  }
-}
 </style>
--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -1,44 +1,57 @@
 <template>
   <div class="app-container">
+    <div class="filter-container" style="margin-bottom: 20px;">
+      <el-input :aria-placeholder="Class" style="width: 600px;" class="filter-item" />
+      <el-select clearable style="width: 90px" class="filter-item">
+        <el-option/>
+      </el-select>
+      <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" >Search</el-button>
+      <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-edit" >Add</el-button>
+      <el-button v-waves :loading="downloadLoading" class="filter-item" type="primary" icon="el-icon-download" >Export</el-button>
+      <!--<el-checkbox v-model="showReviewer" class="filter-item" style="margin-left:15px;" @change="tableKey=tableKey+1">{{ $t('table.reviewer') }}</el-checkbox>-->
+    </div>
     <el-table
       v-loading="listLoading"
-      :data="list"
+      :data="data2"
       element-loading-text="Loading"
       border
       fit
       highlight-current-row>
-      <el-table-column align="center" label="ID" width="95">
+        <!--<el-checkbox v-model="selectAll" >Option</el-checkbox>-->
+      <el-table-column prop="id" align="center" label="ID" >
+      </el-table-column>
+      <el-table-column label="Name" prop="name" >
+      </el-table-column>
+      <el-table-column label="Class" prop="class" align="center">
+      </el-table-column>
+      <el-table-column label="Grade" prop="grade" align="center">
+      </el-table-column>
+      <el-table-column label="Refer" prop="refer" align="center">
+      </el-table-column>
+      <el-table-column label="Actions" align="center" width="230" class-name="small-padding fixed-width">
         <template slot-scope="scope">
-          {{ scope.$index }}
+          <el-button type="success" size="mini" >Edit</el-button>
+          <el-button size="mini" type="danger" >Delete</el-button>
+          <el-button size="mini" type="info" >Refer</el-button>
         </template>
       </el-table-column>
-      <el-table-column label="Title">
-        <template slot-scope="scope">
-          {{ scope.row.title }}
-        </template>
-      </el-table-column>
-      <el-table-column label="Author" width="110" align="center">
-        <template slot-scope="scope">
-          <span>{{ scope.row.author }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="Pageviews" width="110" align="center">
-        <template slot-scope="scope">
-          {{ scope.row.pageviews }}
-        </template>
-      </el-table-column>
-      <el-table-column class-name="status-col" label="Status" width="110" align="center">
-        <template slot-scope="scope">
-          <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
-        <template slot-scope="scope">
-          <i class="el-icon-time"/>
-          <span>{{ scope.row.display_time }}</span>
-        </template>
-      </el-table-column>
+      <!--<el-table-column align="center" prop="created_at" label="Display_time" width="200">-->
+        <!--<template slot-scope="scope">-->
+          <!--<i class="el-icon-time"/>-->
+          <!--<span>{{ scope.row.display_time }}</span>-->
+        <!--</template>-->
+      <!--</el-table-column>-->
     </el-table>
+    <div style="position:absolute; bottom:0; width:100%; height:60px; margin-left:300px">
+      <el-pagination
+        :current-page.sync="currentPage4"
+        :page-sizes="[100, 200, 300, 400]"
+        :page-size="100"
+        :total="400"
+        layout="total, sizes, prev, pager, next, jumper"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"/>
+    </div>
   </div>
 </template>
 
@@ -58,8 +71,46 @@ export default {
   },
   data() {
     return {
+      total: 3,
+      listLoading: true,
+      listQuery: {
+        page: 1,
+        limit: 20,
+        importance: undefined,
+        title: undefined,
+        type: undefined,
+        sort: '+id'
+      },
+      importanceOptions: [1, 2, 3],
+      sortOptions: [{ label: 'ID Ascending', key: '+id' }, { label: 'ID Descending', key: '-id' }],
+      statusOptions: ['published', 'draft', 'deleted'],
+      showReviewer: false,
       list: null,
-      listLoading: true
+      selectAll: false,
+      data2: [{
+        selected: false,
+        id: 1,
+        name: 'George',
+        class: 'classWillow',
+        grade: 'gradeOne',
+        refer: 'DvA'
+      },
+        {
+          selected: false,
+          id: 2,
+          name: 'Sarah',
+          class: 'classPine',
+          grade: 'gradeTwo',
+          refer: 'STF'
+        },
+        {
+          selected: false,
+          id: 3,
+          name: 'Donald',
+          class: 'classOak',
+          grade: 'gradeThree',
+          refer: 'DNA'
+        }]
     }
   },
   created() {
@@ -75,4 +126,5 @@ export default {
     }
   }
 }
+
 </script>
