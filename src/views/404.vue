7fd50b05a298bdc91e9d6c06321f455b8869b119
Donald Tran <yosoup@outlook.com>
2019-03-12 21:34:57 -0700

    change Chinese error message to english

--- src/views/404.vue
+++ src/views/404.vue
@@ -9,32 +9,30 @@
       </div>
       <div class="bullshit">
         <div class="bullshit__oops">OOPS!</div>
-        <div class="bullshit__info">版权所有
-          <a class="link-type" href="https://wallstreetcn.com" target="_blank">华尔街见闻</a>
-        </div>
         <div class="bullshit__headline">{{ message }}</div>
-        <div class="bullshit__info">请检查您输入的网址是否正确，请点击以下按钮返回主页或者发送错误报告</div>
-        <a href="" class="bullshit__return-home">返回首页</a>
+        <div
+          class="bullshit__info"
+        >But a team of highly trained graduate students have been dispatched to handle this error!</div>
+        <a href class="bullshit__return-home">Return Home</a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'Page404',
   computed: {
     message() {
-      return '网管说这个页面你不能进......'
+      return 'Something went wrong...'
     }
   }
 }
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-.wscn-http404-container{
-  transform: translate(-50%,-50%);
+.wscn-http404-container {
+  transform: translate(-50%, -50%);
   position: absolute;
   top: 40%;
   left: 50%;
